10fc07ccee2920f3d5f9a55e3a8dab644883622e
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-03-30 15:31:30 +0300

    Member Entity created

--- src/main/java/com/turkcell/library/system/entities/Member.java
+++ src/main/java/com/turkcell/library/system/entities/Member.java
@@ -1,4 +1,22 @@
 package com.turkcell.library.system.entities;
 
-public class Member {
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "members")
+public class Member extends User {
+    @OneToMany(mappedBy = "member")
+    private List<Rental> rentals;
 }
