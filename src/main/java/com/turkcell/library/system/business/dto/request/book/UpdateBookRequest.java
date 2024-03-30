75ef04a4d3253535be5d96e2fcae2df4e62fb434
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-03-30 16:12:10 +0300

    Update book request DTO created

---
+++ src/main/java/com/turkcell/library/system/business/dto/request/book/UpdateBookRequest.java
@@ -0,0 +1,20 @@
+package com.turkcell.library.system.business.dto.request.book;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+//Via Dto request object, end user db access restricted
+public class UpdateBookRequest {
+    //with id field repository infer this process is an update process
+    private int id;
+    private String name;
+    private String author;
+    private boolean available;
+}
