b2ada13a553351457821c90ed1bee26d9a4c6de8
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-03-30 16:16:19 +0300

    Get All book response DTO created

---
+++ src/main/java/com/turkcell/library/system/business/dto/response/book/GetAllBookResponse.java
@@ -0,0 +1,18 @@
+package com.turkcell.library.system.business.dto.response.book;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+//Via Dto request object, end user db access restricted
+public class GetAllBookResponse {
+    private int id;
+    private String name;
+    private String author;
+    private boolean available;
+}
