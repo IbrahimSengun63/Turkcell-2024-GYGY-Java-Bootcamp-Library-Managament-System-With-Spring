2aa0cd903653c0bb3fb37bb5ae618d558fc15050
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-03-30 16:15:09 +0300

    Get By Id book response DTO created

---
+++ src/main/java/com/turkcell/library/system/business/dto/response/book/GetByIdBookResponse.java
@@ -0,0 +1,18 @@
+package com.turkcell.library.system.business.dto.response.book;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class GetByIdBookResponse {
+    private String name;
+    private String author;
+    private boolean available;
+}
