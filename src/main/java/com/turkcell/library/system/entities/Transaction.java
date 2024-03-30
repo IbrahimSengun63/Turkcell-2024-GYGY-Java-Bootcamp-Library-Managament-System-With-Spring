ade48e3828045a3365f0bd2732cf0d73cb9e6e7a
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-03-30 15:33:18 +0300

    Transaction Entity created

--- src/main/java/com/turkcell/library/system/entities/Transaction.java
+++ src/main/java/com/turkcell/library/system/entities/Transaction.java
@@ -1,4 +1,33 @@
 package com.turkcell.library.system.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "transactions")
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "debt")
+    private Double debt;
+
+    @Column(name = "operation")
+    private int operation;
+
+    @ManyToOne()
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+    @ManyToOne()
+    @JoinColumn(name = "rental_id")
+    private Rental rental;
 }
