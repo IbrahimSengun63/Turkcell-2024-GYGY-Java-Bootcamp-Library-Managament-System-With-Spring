facacd8bc2af4ad0574b6200e60cfd572af34315
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-03-30 15:26:00 +0300

    Book Entity created

---
+++ src/main/java/com/turkcell/library/system/entities/Book.java
@@ -0,0 +1,31 @@
+package com.turkcell.library.system.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "books")
+
+//Book table defined
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "available")
+    private boolean available;
+
+}
