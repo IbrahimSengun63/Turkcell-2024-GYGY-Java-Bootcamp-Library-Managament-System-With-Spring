ad9bdcad934d552ac943d08729d2f09167618375
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-03-30 16:03:57 +0300

    Book Repository created

---
+++ src/main/java/com/turkcell/library/system/dataAccess/abstracts/BookRepository.java
@@ -0,0 +1,8 @@
+package com.turkcell.library.system.dataAccess.abstracts;
+
+import com.turkcell.library.system.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//jpa db built in methods accessed by extending jpa repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
