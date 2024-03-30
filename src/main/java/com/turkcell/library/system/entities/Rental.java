3d5ed9cbe47c23df549eaa9017fa375bfd7f65f1
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-03-30 15:32:29 +0300

    Rental Entity created

--- src/main/java/com/turkcell/library/system/entities/Rental.java
+++ src/main/java/com/turkcell/library/system/entities/Rental.java
@@ -1,4 +1,39 @@
 package com.turkcell.library.system.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "rentals")
 public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @ManyToOne()
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne()
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @OneToMany(mappedBy = "rental")
+    private List<Transaction> transactions;
 }
