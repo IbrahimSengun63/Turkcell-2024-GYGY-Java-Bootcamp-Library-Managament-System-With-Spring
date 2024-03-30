da3c933b9e0802b9c49964f649d66720cb80f709
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-03-30 16:56:47 +0300

    Member business rules defined in repository

--- src/main/java/com/turkcell/library/system/dataAccess/abstracts/MemberRepository.java
+++ src/main/java/com/turkcell/library/system/dataAccess/abstracts/MemberRepository.java
@@ -4,4 +4,5 @@ import com.turkcell.library.system.entities.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member,Integer> {
+    boolean existsByName(String name);
 }
