4d034f17d81cc2590eae40393acfc2b8dc358a99
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-03-30 16:47:57 +0300

    Get By Id member response DTO created

---
+++ src/main/java/com/turkcell/library/system/business/dto/response/member/GetByIdMemberResponse.java
@@ -0,0 +1,16 @@
+package com.turkcell.library.system.business.dto.response.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class GetByIdMemberResponse {
+    private int id;
+    private String name;
+}
