d97c8916949da5370b3ac744de552f3aa6f39672
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-03-30 15:30:38 +0300

    Employee Entity created

--- src/main/java/com/turkcell/library/system/entities/Employee.java
+++ src/main/java/com/turkcell/library/system/entities/Employee.java
@@ -1,4 +1,23 @@
 package com.turkcell.library.system.entities;
 
-public class Employee {
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee extends User {
+
+    @OneToMany(mappedBy = "employee")
+    private List<Transaction> transactions;
 }
