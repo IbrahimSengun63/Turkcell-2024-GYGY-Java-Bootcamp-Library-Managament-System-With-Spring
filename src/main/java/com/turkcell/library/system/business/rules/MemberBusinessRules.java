ab204f084673bb42b9ba16313f3daf5fb5ccddb7
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-03-30 16:55:52 +0300

    Member business rules created

---
+++ src/main/java/com/turkcell/library/system/business/rules/MemberBusinessRules.java
@@ -0,0 +1,18 @@
+package com.turkcell.library.system.business.rules;
+
+import com.turkcell.library.system.core.utilities.exceptions.BusinessException;
+import com.turkcell.library.system.dataAccess.abstracts.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MemberBusinessRules {
+    private final MemberRepository memberRepository;
+
+    public void checkIfMemberNameExists(String name) {
+        if (this.memberRepository.existsByName(name)) {
+            throw new BusinessException("Member already exists");
+        }
+    }
+}
