dc6e5e2bce0ef163084189be0c01a53414b2a4b5
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-03-30 16:08:01 +0300

    Add book request DTO created

---
+++ src/main/java/com/turkcell/library/system/business/dto/request/AddBookRequest.java
@@ -0,0 +1,24 @@
+package com.turkcell.library.system.business.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+//Via Dto request object, end user db access restricted
+public class AddBookRequest {
+    // field validations defined
+    // to make it active this validations in web api layer at the related end point we must define @Valid annotations as well
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String name;
+    private String author;
+    private boolean available;
+}
