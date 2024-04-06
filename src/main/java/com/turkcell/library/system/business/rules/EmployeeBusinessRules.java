fb68bef2e684be321ae190ef26f303847ce6b177
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-04-06 22:30:38 +0300

    Employee business rule created.

---
+++ src/main/java/com/turkcell/library/system/business/rules/EmployeeBusinessRules.java
@@ -0,0 +1,17 @@
+package com.turkcell.library.system.business.rules;
+
+import com.turkcell.library.system.core.utilities.exceptions.BusinessException;
+import com.turkcell.library.system.dataAccess.abstracts.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class EmployeeBusinessRules {
+    private final EmployeeRepository employeeRepository;
+    public void checkIfEmployeeNameExists(String name) {
+        if (this.employeeRepository.existsByName(name)){
+            throw new BusinessException("Employee already exists.");
+        }
+    }
+}
