dec9de4a221ed19ae349c9de116b512099614152
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-04-07 00:47:47 +0300

    Update rental response DTO created.

---
+++ src/main/java/com/turkcell/library/system/business/dto/response/rental/UpdateRentalResponse.java
@@ -0,0 +1,20 @@
+package com.turkcell.library.system.business.dto.response.rental;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateRentalResponse {
+    private int id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private int memberId;
+    private int bookId;
+}
