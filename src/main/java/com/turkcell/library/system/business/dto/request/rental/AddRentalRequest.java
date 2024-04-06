aa2de35074aebabcc05697396a6cb45e2d613cea
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-04-07 00:42:21 +0300

    Add rental DTO modified.

---
+++ src/main/java/com/turkcell/library/system/business/dto/request/rental/AddRentalRequest.java
@@ -0,0 +1,20 @@
+package com.turkcell.library.system.business.dto.request.rental;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddRentalRequest {
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private int memberId;
+    private int bookId;
+}
