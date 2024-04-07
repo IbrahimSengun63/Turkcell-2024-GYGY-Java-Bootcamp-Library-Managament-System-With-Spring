29a3c56b7c90d5bd51e3d681eddf6b29384d074d
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-04-07 16:25:09 +0300

    Rental mapper get all modified.

--- src/main/java/com/turkcell/library/system/core/utilities/mappers/RentalMapper.java
+++ src/main/java/com/turkcell/library/system/core/utilities/mappers/RentalMapper.java
@@ -9,6 +9,7 @@ import com.turkcell.library.system.business.dto.response.rental.UpdateRentalResp
 import com.turkcell.library.system.entities.Rental;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -18,13 +19,11 @@ import java.util.stream.Collectors;
 public interface RentalMapper {
     RentalMapper INSTANCE = Mappers.getMapper(RentalMapper.class);
 
-
     Rental rentalFromAddRequest(AddRentalRequest request);
 
     @Mapping(source = "member.id", target = "memberId")
     @Mapping(source = "book.id", target = "bookId")
     AddRentalResponse AddRentalResponseFromRental(Rental rental);
-
 
     Rental rentalFromUpdateRequest(UpdateRentalRequest request);
 
@@ -42,5 +41,7 @@ public interface RentalMapper {
     @Mapping(source = "member.name", target = "memberName")
     @Mapping(source = "book.id", target = "bookId")
     @Mapping(source = "book.name", target = "bookName")
+    GetAllRentalResponse rentalToGetAllRentalResponse(Rental rental);
+
     List<GetAllRentalResponse> getAllRentalResponseFromRentals(List<Rental> rentals);
 }
