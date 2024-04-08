d95efc6d1ebe39c9315be39a7133149859b7ddab
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-04-08 13:08:17 +0300

    Add transaction request created.

---
+++ src/main/java/com/turkcell/library/system/business/dto/request/transaction/AddTransactionRequest.java
@@ -0,0 +1,20 @@
+package com.turkcell.library.system.business.dto.request.transaction;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddTransactionRequest {
+    //todo: use default value anno
+    private double debt;
+    @NotNull
+    private int operation;
+    private int employeeId;
+    private int rentalId;
+}
