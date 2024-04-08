8413d9a7f428325fdc52c51cac0da3764932dba0
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-04-08 13:29:59 +0300

    Get All transaction response dto created.

---
+++ src/main/java/com/turkcell/library/system/business/dto/response/transaction/GetAllTransactionResponse.java
@@ -0,0 +1,23 @@
+package com.turkcell.library.system.business.dto.response.transaction;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetAllTransactionResponse {
+    private int id;
+    private double debt;
+    private int operation;
+    private int employeeId;
+    private String employeeName;
+    private int rentalId;
+    private int memberId;
+    private String memberName;
+    private int bookId;
+    private String bookName;
+}
