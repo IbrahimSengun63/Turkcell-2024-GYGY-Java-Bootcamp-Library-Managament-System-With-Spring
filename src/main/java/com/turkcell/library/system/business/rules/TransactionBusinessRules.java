35e053017b18a31b20fa69dd56d298137f0e1fbf
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-04-10 13:11:46 +0300

    Transaction business rule modified.

--- src/main/java/com/turkcell/library/system/business/rules/TransactionBusinessRules.java
+++ src/main/java/com/turkcell/library/system/business/rules/TransactionBusinessRules.java
@@ -1,13 +1,33 @@
 package com.turkcell.library.system.business.rules;
 
 import com.turkcell.library.system.core.utilities.exceptions.BusinessException;
+import com.turkcell.library.system.dataAccess.abstracts.EmployeeRepository;
+import com.turkcell.library.system.dataAccess.abstracts.MemberRepository;
+import com.turkcell.library.system.dataAccess.abstracts.RentalRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TransactionBusinessRules {
+    private final RentalRepository rentalRepository;
+    private final EmployeeRepository employeeRepository;
+
     public void checkIfDebtNormal(Double debt) {
         if (debt < 0) {
             throw new BusinessException("Debt can not be negative");
+        }
+    }
+
+    public void checkIfRentalExists(int id) {
+        if (this.rentalRepository.findById(id).isEmpty()) {
+            throw new BusinessException("Rental with " + id + " id couldn't be found");
+        }
+    }
+
+    public void checkIfEmployeeExists(int id) {
+        if (this.employeeRepository.findById(id).isEmpty()) {
+            throw new BusinessException("Employee with " + id + " id couldn't be found");
         }
     }
 }
